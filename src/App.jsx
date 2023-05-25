0833d83ab7572de7ea0461f9a520cb93b29d533a
Hiago Emanuel <hiagoemanuel321@hotmail.com>
2023-05-25 09:10:29 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Reset } from "./styles/reset"
+import { Variables } from "./styles/variables"
+import { GlobalStyle } from "./styles/global-style"
+
+function App() {
+  return (
+    <>
+      <Reset />
+      <Variables />
+      <GlobalStyle />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/variables.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Variables = createGlobalStyle`
+    :root {
+        --primary-color: #1B2223;
+        --secondary-color: #3A4F50;
+        --tertiary-color: #0EF6CC;
+        --quaternary-color: #F4FEFD;
+    }
+`
